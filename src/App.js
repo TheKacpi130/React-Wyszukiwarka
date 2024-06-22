e6c174dfe0f130299958eab12591a14b676a6dc8
TheKacpi130 <thekacpi130@gmail.com>
2024-06-22 19:46:04 +0200

    giga poprawka #1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Searchbar } from "./Searchbar.js";
+import { RecipesList } from "./RecipesList.js";
+import Header from "./Header.js";
+import { useState } from "react";
+
+function checkWindowSize() {
+  if (window.innerWidth < 600) {
+    document.body.style.backgroundColor = "lightblue";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+}
+window.addEventListener('resize', checkWindowSize);
+checkWindowSize();
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  console.log("Recipes in App:", recipes);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Searchbar setRecipes={setRecipes} />
+      <RecipesList recipes={recipes} />
     </div>
   );
 }
